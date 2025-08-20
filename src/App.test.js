3cf72dc0f6a647eecaca2287e3795b5a812a955f
davidzamora9aSyC <79547839+davidzamora9aSyC@users.noreply.github.com>
2025-08-19 22:35:37 -0500

    feat: add expandable overlays to dashboard widgets

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders nueva minuta heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/nueva minuta/i);
+  expect(heading).toBeInTheDocument();
 });
